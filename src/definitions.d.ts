34220b9c8bf3f68c896842640316cfc01db602a8
mano92fuentesjimenez <m92fuentes@gmail.com>
2021-01-18 14:23:45 -0500

    fix type declarations

--- src/definitions.d.ts
+++ src/definitions.d.ts
@@ -1,50 +1,54 @@
 import { Map } from "leaflet";
-declare module "leaflet-timedimension-scoped" {
-  export class TimeDimensionControl {
-    constructor(options: { dog: boolean });
-  }
 
-  export interface TimeDimensionOptions {
-    loadingTimeout?: number;
-    currentTime?: string;
-    // time period with ISO 8601
-    times?: string;
-    lowerLimitTime?: Date;
-    upperLimitTime?: Date;
-  }
+export class TimeDimensionControl {
+  constructor(options: { dog: boolean });
+}
 
-  export class TimeDimension {
-    constructor(options: TimeDimensionOptions)
-  }
+export interface TimeDimensionOptions {
+  loadingTimeout?: number;
+  currentTime?: string;
+  // time period with ISO 8601
+  times?: string;
+  lowerLimitTime?: Date;
+  upperLimitTime?: Date;
+}
 
-  export interface TimeDimensionWMS {
-    _update: () => void;
-    onRemove: (map: Map) => void;
-    setLoaded: (loaded: boolean) => void;
-    isLoaded: () => boolean;
-    hide: () => void;
-    show: () => void;
-    getURL: () => string;
-  }
+export class TimeDimension {
+  constructor(options: TimeDimensionOptions);
+}
 
-  export interface TimeDimensionWMSLayerOptions {
-    getCapabilitiesParams?: any;
-    getCapabilitiesUrl?: string;
-    getCapabilitiesLayerName?: string;
-    cache?: number;
-    cacheBackward?: number;
-    cacheForward?: number;
-    wmsVersion?: string;
-    requestTimeFromCapabilities?: boolean;
-    timeDimension: TimeDimension;
-  }
+export interface TimeDimensionWMS {
+  _update: () => void;
+  onRemove: (map: Map) => void;
+  setLoaded: (loaded: boolean) => void;
+  isLoaded: () => boolean;
+  hide: () => void;
+  show: () => void;
+  getURL: () => string;
+}
 
+export interface TimeDimensionWMSLayerOptions {
+  getCapabilitiesParams?: any;
+  getCapabilitiesUrl?: string;
+  getCapabilitiesLayerName?: string;
+  cache?: number;
+  cacheBackward?: number;
+  cacheForward?: number;
+  wmsVersion?: string;
+  requestTimeFromCapabilities?: boolean;
+  timeDimension: TimeDimension;
+}
+
+export class TimeDimensionWMSLayer {
+  constructor(
+    wmsLayer: TimeDimensionWMS,
+    options: TimeDimensionWMSLayerOptions
+  );
+}
+
+declare module "leaflet" {
   export interface MapOptions {
-    timeDimension: boolean,
-    timeDimensionControl: boolean,
-  }
-
-  export class TimeDimensionWMSLayer {
-    constructor(wmsLayer: TimeDimensionWMS, options: TimeDimensionWMSLayerOptions);
+    timeDimension?: boolean;
+    timeDimensionControl?: boolean;
   }
 }
