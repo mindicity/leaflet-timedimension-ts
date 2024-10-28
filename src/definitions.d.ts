d941f111ac72945a9b3c89858d76d052bfbfe0ea
Alessio Massetti <a.massetti@fabbricadigitale.it>
2024-10-28 15:36:32 +0100

    Fix defintions

--- src/definitions.d.ts
+++ src/definitions.d.ts
@@ -1,136 +1,138 @@
-import { Map, Control, ControlOptions, Evented } from "leaflet";
+import {Map, Control, ControlOptions, Evented} from "leaflet";
 
 export class TimeDimensionControl extends Control {
-  constructor(options: { dog: boolean });
+    constructor(options: TimeDimensionControlOptions);
 }
 
 export interface PlayerOptions {
-  buffer?: number;
-  minBufferReady?: number;
-  loop?: boolean;
-  transitionTime?: number;
+    buffer?: number;
+    minBufferReady?: number;
+    loop?: boolean;
+    transitionTime?: number;
 }
 
 export class Player extends Evented {
-  constructor(options: PlayerOptions, timeDimension: TimeDimension);
-  start: (numSteps: number) => void;
-  stop: () => void;
+    constructor(options: PlayerOptions, timeDimension: TimeDimension);
+
+    start: (numSteps: number) => void;
+    stop: () => void;
 }
 
 export interface TimeDimensionOptions {
-  loadingTimeout?: number;
-  currentTime?: string;
-  // time period with ISO 8601
-  times?: string;
-  lowerLimitTime?: Date;
-  upperLimitTime?: Date;
-  player?: Player;
-  playerOptions?: PlayerOptions;
+    loadingTimeout?: number;
+    currentTime?: string;
+    // time period with ISO 8601
+    times?: string;
+    lowerLimitTime?: Date;
+    upperLimitTime?: Date;
+    player?: Player;
+    playerOptions?: PlayerOptions;
 }
 
 export class TimeDimension extends Evented {
-  constructor(options: TimeDimensionOptions);
-  setAvailableTimes: (times: string, mode: 'replace'| 'intersect' | 'extremes'| 'union' ) => void;
-  setCurrentTime:(time: number) => void;
-  getCurrentTime:() => number | null;
-  prepareNextTimes: (numSteps: number, howMany: number, loop: boolean) => void;
+    constructor(options: TimeDimensionOptions);
+
+    setAvailableTimes: (times: string, mode: 'replace' | 'intersect' | 'extremes' | 'union') => void;
+    setCurrentTime: (time: number) => void;
+    getCurrentTime: () => number | null;
+    prepareNextTimes: (numSteps: number, howMany: number, loop: boolean) => void;
 }
 
 export interface TimeDimensionWMS {
-  _update: () => void;
-  onRemove: (map: Map) => void;
-  setLoaded: (loaded: boolean) => void;
-  isLoaded: () => boolean;
-  hide: () => void;
-  show: () => void;
-  getURL: () => string;
+    _update: () => void;
+    onRemove: (map: Map) => void;
+    setLoaded: (loaded: boolean) => void;
+    isLoaded: () => boolean;
+    hide: () => void;
+    show: () => void;
+    getURL: () => string;
 }
 
 export interface TimeDimensionWMSLayerOptions {
-  getCapabilitiesParams?: any;
-  getCapabilitiesUrl?: string;
-  getCapabilitiesLayerName?: string;
-  cache?: number;
-  cacheBackward?: number;
-  cacheForward?: number;
-  wmsVersion?: string;
-  requestTimeFromCapabilities?: boolean;
-  timeDimension?: TimeDimension;
+    getCapabilitiesParams?: any;
+    getCapabilitiesUrl?: string;
+    getCapabilitiesLayerName?: string;
+    cache?: number;
+    cacheBackward?: number;
+    cacheForward?: number;
+    wmsVersion?: string;
+    requestTimeFromCapabilities?: boolean;
+    timeDimension?: TimeDimension;
 }
 
 export class TimeDimensionWMSLayer {
-  constructor(
-    wmsLayer: TimeDimensionWMS,
-    options: TimeDimensionWMSLayerOptions
-  );
+    constructor(
+        wmsLayer: TimeDimensionWMS,
+        options: TimeDimensionWMSLayerOptions
+    );
 }
 
 export interface TimeDimensionImage {
-  _update: () => void;
-  onRemove: (map: Map) => void;
-  setLoaded: (loaded: boolean) => void;
-  isLoaded: () => boolean;
-  hide: () => void;
-  show: () => void;
-  getURL: () => string;
+    _update: () => void;
+    onRemove: (map: Map) => void;
+    setLoaded: (loaded: boolean) => void;
+    isLoaded: () => boolean;
+    hide: () => void;
+    show: () => void;
+    getURL: () => string;
 }
 
 export interface TimeDimensionImageLayerOptions {
-  getCapabilitiesParams?: any;
-  getCapabilitiesUrl?: string;
-  getCapabilitiesLayerName?: string;
-  cache?: number;
-  cacheBackward?: number;
-  cacheForward?: number;
-  requestTimeFromCapabilities?: boolean;
-  timeDimension?: TimeDimension;
+    getCapabilitiesParams?: any;
+    getCapabilitiesUrl?: string;
+    getCapabilitiesLayerName?: string;
+    cache?: number;
+    cacheBackward?: number;
+    cacheForward?: number;
+    requestTimeFromCapabilities?: boolean;
+    timeDimension?: TimeDimension;
 }
 
 export class TimeDimensionImageLayer {
-  constructor(
-    imageLayer: TimeDimensionImage,
-    options: TimeDimensionImageLayerOptions
-  );
+    constructor(
+        imageLayer: TimeDimensionImage,
+        options: TimeDimensionImageLayerOptions
+    );
 }
 
 export interface TimeDimensionControlOptions extends ControlOptions {
-  onlyUTC?: boolean,
-  timeZones?: string[],
-  minSpeed?: number,
-  maxSpeed?: number,
-  speedStep?: number,
-  timeSteps?: number,
-  timeSlider?: boolean,
-  timeSliderDragUpdate?: boolean,
-  limitSliders?: boolean,
-  limitMinimumRange?: number,
-  speedSlider?: boolean,
-  autoPlay?: boolean,
-  backwardButton?: boolean,
-  forwardButton?: boolean,
-  playButton?: boolean,
-  playReverseButton?: boolean,
-  loopButton?: boolean,
-  displayDate?: boolean,
-  dateFontWeight?: number,
-  title?: string,
-  styleNS?: string,
-  player?: Player,
-  playerOptions?: PlayerOptions;
+    onlyUTC?: boolean,
+    timeZones?: string[],
+    minSpeed?: number,
+    maxSpeed?: number,
+    speedStep?: number,
+    timeSteps?: number,
+    timeSlider?: boolean,
+    timeSliderDragUpdate?: boolean,
+    limitSliders?: boolean,
+    limitMinimumRange?: number,
+    speedSlider?: boolean,
+    autoPlay?: boolean,
+    backwardButton?: boolean,
+    forwardButton?: boolean,
+    playButton?: boolean,
+    playReverseButton?: boolean,
+    loopButton?: boolean,
+    displayDate?: boolean,
+    dateFontWeight?: number,
+    title?: string,
+    styleNS?: string,
+    player?: Player,
+    playerOptions?: PlayerOptions;
 }
 
 export function parseTimesExpression(times: string)
 
 declare module "leaflet" {
-  export interface MapOptions {
-    timeDimension?: boolean | TimeDimension;
-    timeDimensionControl?: boolean;
-    timeDimensionControlOptions?: TimeDimensionControlOptions;
-  }
+    export interface MapOptions {
+        timeDimension?: boolean | TimeDimension;
+        timeDimensionControl?: boolean;
+        timeDimensionControlOptions?: TimeDimensionControlOptions;
+    }
 
-  export class Map {
-    public timeDimension: TimeDimension;
-    public TimeDimensionPlayer: Player;
-  }
+    export class Map {
+        public timeDimension: TimeDimension;
+        public TimeDimensionPlayer: Player;
+    }
 }
 
